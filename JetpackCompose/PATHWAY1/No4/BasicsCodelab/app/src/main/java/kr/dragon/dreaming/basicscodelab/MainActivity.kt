9ee5c4dbaf3836cb82a14a157c6d20b872fbb577
DreamingDragon <thedreamingd@gmail.com>
2026-06-05 23:23:28 +0900

    Android 개발자를 위한 Jetpack Compose 개발자 과정 1: Compose 기본사항 4. 첫 번째 Compose 앱 만들기 6. 열과 행 만들기 - 진행중

--- JetpackCompose/PATHWAY1/No4/BasicsCodelab/app/src/main/java/kr/dragon/dreaming/basicscodelab/MainActivity.kt
+++ JetpackCompose/PATHWAY1/No4/BasicsCodelab/app/src/main/java/kr/dragon/dreaming/basicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -41,10 +42,10 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "Hello ")
+            Text(text = name)
+        }
     }
 }
 
