f5f64e8b3ea6c0983f1a6f57a293bce0fc9672b8
DreamingDragon <thedreamingd@gmail.com>
2026-08-18 00:23:00 +0900

    Android Basics with Compose Unit 1: Your first Android app Build a basic layout

---
+++ AndroidBasics/Unit1/PATHWAY3/HappyBirthday/app/src/main/java/kr/dragon/dreaming/happybirthday/MainActivity.kt
@@ -0,0 +1,52 @@
+package kr.dragon.dreaming.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.dragon.dreaming.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+/*
+ * 컴포즈함수 이름은 명사
+ */
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true,
+    name = "My Preview")
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        Greeting("Android")
+    }
+}
