e180ae0fc3f103d14f5eb3732a8f3a682d494bb8
DreamingDragon <thedreamingd@gmail.com>
2026-08-20 14:58:33 +0900

    Android Basics with Compose Unit 2: Building app UI Interacting with UI and state

--- AndroidBasics/Unit2/PATHWAY3/tip-calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ AndroidBasics/Unit2/PATHWAY3/tip-calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -90,10 +92,15 @@ fun TipTimeLayout() {
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
 
     TextField(
         value = amountInput,
         onValueChange = { amountInput = it },
+        label = { Text(stringResource(R.string.bill_amount)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
     )
 }
