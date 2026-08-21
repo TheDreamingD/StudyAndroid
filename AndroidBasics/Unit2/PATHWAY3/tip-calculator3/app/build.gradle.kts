28769f5983effdda32b91ff538fd4af76f02c95e
DreamingDragon <thedreamingd@gmail.com>
2026-08-21 13:25:38 +0900

    Android Basics with Compose Unit 2: Building app UI Interacting with UI and state

--- AndroidBasics/Unit2/PATHWAY3/tip-calculator3/app/build.gradle.kts
+++ AndroidBasics/Unit2/PATHWAY3/tip-calculator3/app/build.gradle.kts
@@ -82,4 +82,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
