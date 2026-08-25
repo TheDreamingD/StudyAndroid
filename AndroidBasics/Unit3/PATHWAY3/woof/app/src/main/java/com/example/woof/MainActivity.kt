e352fef24492dbfe85666d12f2964f06f2d71cd8
DreamingDragon <thedreamingd@gmail.com>
2026-08-26 01:11:59 +0900

    Android Basics with Compose Unit 3: Display lists and use Material Design Build beautiful apps

--- AndroidBasics/Unit3/PATHWAY3/woof/app/src/main/java/com/example/woof/MainActivity.kt
+++ AndroidBasics/Unit3/PATHWAY3/woof/app/src/main/java/com/example/woof/MainActivity.kt
@@ -31,13 +31,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -67,11 +72,18 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun WoofApp() {
-    LazyColumn {
-        items(dogs) {
-            DogItem(dog = it,
-                modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
-            )
+    // Scaffold는 다양한 구성요소와 화면 요소의 슬롯을 제공하는 레이아웃
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) { it -> // ContentWindowInsets 매개변수를 지원한다. WindowInsets는 앱이 시스템UI와 교차할수있는 부분으로 PaddingValues 매개변수를 통해 콘텐츠 슬롯에 전달된다.
+        LazyColumn(contentPadding = it) {
+            items(dogs) {
+                DogItem(dog = it,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
         }
     }
 }
@@ -150,6 +162,31 @@ fun DogInformation(
             style = MaterialTheme.typography.bodyLarge // 폰트를 변경해줌
         )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_woof_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
 /**
