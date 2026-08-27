d23e40c7940be13f699790c5c25d6d890597c4e8
DreamingDragon <thedreamingd@gmail.com>
2026-08-27 13:35:40 +0900

    Android Basics with Compose Unit 4: Navigation and app architecture Architecture Components

---
+++ AndroidBasics/Unit4/PATHWAY1/unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.unscramble.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+    // state flow란 현재 상태와 새로운 상태를 관찰할 수 있는 데이터 홀더 플로우
+    // 상태를 업데이트하고 흐름을 전송하려면 여기에 새로운 값을 넣으면 된다.
+    private val _uiState = MutableStateFlow(GameUiState()) // viewmodel 내에서만 접근할 수 있다.
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow() // 외부에서는 이 변수를 통해 값을 읽어간다. asStateFLow는 상태 흐름을 읽기 전용으로 만든다.
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    var userGuess by mutableStateOf("")
+        private set
+
+    init {
+        resetGame()
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    fun updateUserGuess(guessedWord: String){
+        userGuess = guessedWord
+    }
+}
