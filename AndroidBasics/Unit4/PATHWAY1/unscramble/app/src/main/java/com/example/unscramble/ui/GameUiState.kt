2e403008935d7d260fd2826c7a26b7147d9de8c1
DreamingDragon <thedreamingd@gmail.com>
2026-08-27 14:47:11 +0900

    Android Basics with Compose Unit 4: Navigation and app architecture Architecture Components

--- AndroidBasics/Unit4/PATHWAY1/unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ AndroidBasics/Unit4/PATHWAY1/unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -4,5 +4,6 @@ data class GameUiState(
     val currentScrambledWord: String = "",
     val isGuessedWordWrong: Boolean = false,
     val currentWordCount: Int = 1,
-    val score: Int = 0
+    val score: Int = 0,
+    val isGameOver: Boolean = false
 )
